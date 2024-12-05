fd71f586a0c26c7bf862a7c03bfb1d953da83a1a
JaydevSR <jaydevsrao@gmail.com>
2024-12-05 13:40:30 +0100

    add deflation and rayleigh quotient iteration

--- src/testbed/diagonalisation.jl
+++ src/testbed/diagonalisation.jl
@@ -1,3 +1,11 @@
+#? Ref: https://jaydevsr.me/computational-physics-notes/preliminaries/eigenvalue-problem.html
+#? After timing:
+#? - `powiter` is faster for the largest eigenvalue that `eigensolve` from KrylovKit.jl (about 5x faster)
+#? - `deflation` scales worse than `eigensolve` from KrylovKit.jl so for more than one exremal eigenvalue it is better to use the latter
+#? It could be possible to improve the perfomance of `deflation`
+
+#! A serious problem, the eigenvalues for a real valued matrix can be complex if it is not symmetric, the code doesn't allow for this
+
 rayleigh_quotient(A::AbstractArray{T, 2}, x::Vector) where T = dot(x, A, x) / dot(x, x)
 
 """Calculate the dominant eigenvalue and corresponding eigenvector of a diagonalisable (eg. Hermitian) matrix"""
@@ -28,3 +36,37 @@ function ipowiter(A::AbstractArray{T, 2}; tol::Float64=1e-6, start_vector=nothin
     return inv(ei), xi  # eigenvalue, eigenvector
 end
 
+"""Calculate the eigenvalue and eigenvector pair closest to a given guess eigenvalue for a diagonalisable matrix"""
+function rqiter(A::AbstractArray{T, 2}, guess_eval::T; tol::Float64=1e-6, start_vector=nothing) where T
+    n, m = size(A)
+    n != m ? throw(ArgumentError("Input must be a square matrix.")) : nothing
+
+    Aμ = A - guess_eval * I
+    eμ, xμ = ipowiter(Aμ; tol, start_vector)
+    return eμ + guess_eval, xμ  # eigenvalue, eigenvector
+end
+
+
+"""Calculate the first `num_vals` eigenvalues and eigenvectors of a Hermitian matrix using the method of deflation.
+    
+    This method uses a `solver` function to determine the particular eigenvalue eigenvector pair at each step which is 
+    then projected out for the next step. Any additional arguments for the `solver` function can be passed as keyword 
+    arguments.
+"""
+function deflation(A::Hermitian{T}, num_vals::Int; 
+    solver::Function=powiter,
+    kwargs...) where T
+    n, m = size(A)
+    num_vals > n ? throw(ArgumentError("Number of eigenvalues requested exceeds matrix size.")) : nothing
+
+    vals = zeros(T, num_vals)
+    vecs = zeros(T, n, num_vals)
+    for i in 1:num_vals
+        ei, xi = solver(A; kwargs...)
+        vals[i] = ei
+        vecs[:, i] = xi
+        A -= ei * xi * xi'
+    end
+
+    return vals, vecs
+end
